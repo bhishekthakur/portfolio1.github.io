56b1c06d97bbccedccd13cc1065a3b3c6858b600
Abhishek thakur <93262866+bhishekthakur@users.noreply.github.com>
2024-01-23 15:00:43 +0530

    Update Navbar.js

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -29,13 +29,13 @@ const Navbar = () => {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="project">Project</Link>
+          <Link to="/project">Project</Link>
         </li>
         <li>
-          <Link to="about">About</Link>
+          <Link to="/about">About</Link>
         </li>
         <li>
-          <Link to="contact">Contact</Link>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
       <div className="hamburger" onClick={handleClick}>
